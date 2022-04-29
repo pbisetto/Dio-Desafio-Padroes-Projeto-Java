895475766ed70993b27957102febd6e882a8260f
Paulo Bisetto <100386404+pbisetto@users.noreply.github.com>
2022-04-29 12:25:06 -0300

    Create Contato.java

---
+++ src/desafio/padroes/javadio/facade/Contato.java
@@ -0,0 +1,24 @@
+package one.digitalInovation.gof.facade;
+
+public class Contato {
+    private String email;
+    private String linkedin;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+}
