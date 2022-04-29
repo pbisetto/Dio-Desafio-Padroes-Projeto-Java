50cac414002af40a99dc3d6ad77aaa697d37e22e
Paulo Bisetto <100386404+pbisetto@users.noreply.github.com>
2022-04-29 12:22:59 -0300

    Update Facade.java

--- src/desafio/padroes/javadio/facade/Facade.java
+++ src/desafio/padroes/javadio/facade/Facade.java
@@ -1,13 +1,17 @@
 package desafio.padroes.javadio.facade;
 import subsistema1.crm.CrmService;
 import subsistema2.CepApi;
+import Subsistema3.contato.ContatoApi;
 
 public class Facade {
 
-    public void migrarCliente(String nome, String cep) {
-        String cidade = CepApi.getInstancia().recuperarCidade(cep);
-        String estado = CepApi.getInstancia().recuperarEstado(cep);
+   public void migrarCliente(String cpf, String nome, String cep) {
+		String cidade = CepApi.getInstancia().recuperarCidade(cep);
+		String estado = CepApi.getInstancia().recuperarEstado(cep);
+		Contato contatos = new Contato();
+		contatos.setEmail(ContatoApi.getInstancia().solicitarEmail(cpf));
+		contatos.setLinkedin(ContatoApi.getInstancia().solicitarLinkedin(cpf));
 
-        CrmService.gravarCliente(nome, cep, cidade, estado);
-    }
+		CrmService.gravarCliente(nome, cep, cidade, estado, contatos);
+	}
 }
